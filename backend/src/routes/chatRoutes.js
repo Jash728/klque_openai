c2084fac166dedf712d2f4c4a598145250093476
Jash <shahjash2809@gmail.com>
2024-12-29 14:44:52 +0530

    first commit

--- backend/src/routes/chatRoutes.js
+++ backend/src/routes/chatRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import Chat from '../models/Chat.js';
+import Chat from '../models/chat.js';
 import { v4 as uuidv4 } from 'uuid'; 
 
 const router = express.Router();
